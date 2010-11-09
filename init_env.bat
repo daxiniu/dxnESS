65cf30046238bbc849f2951743c7a9d0ee3f8c98
Zhao Lei <aicoylei@gmail.com>
2010-11-09 16:37:56 +0800

    修改空目录自动生成脚本

--- init_env.bat
+++ init_env.bat
@@ -1,3 +1,7 @@
 mkdir "public\protected\runtime"
 mkdir "public\protected\data"
 mkdir "public\assets"
+
+mkdir "admin\protected\runtime"
+mkdir "admin\protected\data"
+mkdir "admin\assets"
--- init_env.sh
+++ init_env.sh
@@ -6,3 +6,10 @@ mkdir public/assets
 chmod -R 777 public/protected/runtime
 chmod -R 777 public/protected/data
 chmod -R 777 public/assets
+
+mkdir admin/protected/runtime
+mkdir admin/protected/data
+mkdir admin/assets
+chmod -R 777 admin/protected/runtime
+chmod -R 777 admin/protected/data
+chmod -R 777 admin/assets
