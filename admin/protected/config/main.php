57cde5b6a029f1f1d2c5629baeb72e5797952687
Zhao Lei <aicoylei@gmail.com>
2010-11-14 15:28:32 +0800

    dis log component

--- admin/protected/config/main.php
+++ admin/protected/config/main.php
@@ -11,7 +11,7 @@ return array(
 	'language'=>'zh_cn',
 
 	// preloading 'log' component
-	'preload'=>array('log'),
+	// 'preload'=>array('log'),
 
 	// autoloading model and component classes
 	'import'=>array(
